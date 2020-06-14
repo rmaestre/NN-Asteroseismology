173589d19226c3ae4fd68106730808305a57de5e
roberto <roberto.maestre@bbva.com>
2020-06-14 08:03:45 +0200

    Black format

--- astronn/__init__.py
+++ astronn/__init__.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Data(ABC):
     """
     dataset to load stars preprocessed data.
@@ -7,6 +8,62 @@ class Data(ABC):
 
     @abstractmethod
     def load(self, folder):
+        """
+        load all files on a given directory (and recursive directories)
+
+        :return: [description]
+        :rtype: PricingObservation
+        """
+        pass
+
+
+class Model(ABC):
+    """
+    dataset to load stars preprocessed data.
+    """
+
+    @abstractmethod
+    def create(self):
+        """
+        load all files on a given directory (and recursive directories)
+
+        :return: [description]
+        :rtype: PricingObservation
+        """
+        pass
+
+    @abstractmethod
+    def fit(self):
+        """
+        load all files on a given directory (and recursive directories)
+
+        :return: [description]
+        :rtype: PricingObservation
+        """
+        pass
+
+    @abstractmethod
+    def predict(self):
+        """
+        load all files on a given directory (and recursive directories)
+
+        :return: [description]
+        :rtype: PricingObservation
+        """
+        pass
+
+    @abstractmethod
+    def save(self):
+        """
+        load all files on a given directory (and recursive directories)
+
+        :return: [description]
+        :rtype: PricingObservation
+        """
+        pass
+
+    @abstractmethod
+    def load(self):
         """
         load all files on a given directory (and recursive directories)
 
