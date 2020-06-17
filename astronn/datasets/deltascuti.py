26ee20bf2df3e2bf9062e2b2224a8a827b3f3c93
roberto <roberto.maestre@bbva.com>
2020-06-17 21:21:09 +0200

    Deltascuti data sets added. Example in notebook

--- astronn/datasets/deltascuti.py
+++ astronn/datasets/deltascuti.py
@@ -24,29 +24,34 @@ class deltascuti(Data):
         # Process each file
         return self.csv_reader_dataset(glob.glob(folder), batch_size=batch_size)
 
-    def parse_csv_line(self, line, n_inputs=1219):
+    def parse_csv_line(self, line, n_inputs=1220):
         """
         each file will be parsed with this method. Mainly, we read the
         raw data, split it into three dimensions (vector X) and 
-        convert last value of raw data into the expected target
+        convert last value of raw data into the expected target.
+        The star name is also returned
         """
-        defs = [tf.constant(np.nan)] * n_inputs
+        # (n_inputs - 1) as float
+        defs = [tf.constant(np.nan)] * (n_inputs - 1)
+        # First field is a string
+        defs.insert(0, tf.constant("", dtype=tf.string))
+        # Read fields
         fields = tf.io.decode_csv(line, record_defaults=defs)
         # Get DFT, HD and AC
-        x = tf.stack(tf.split(fields[: 406 * 3], 3), axis=-1)  # Split channels
+        x = tf.stack(tf.split(fields[1 : (406 * 3) + 1], 3), axis=-1)  # Split channels
         # Get Dnu (-1) or dr (-2)
-        print(fields[-1:])
-        aux = tf.cast(tf.convert_to_tensor(fields[-1:]) / 0.0864, tf.int32)
-        y = tf.reshape(tf.one_hot(depth=100, indices=aux, axis=0), (1, 100))
-        return x, y
+        y = tf.reshape(
+            tf.one_hot(
+                depth=100,
+                indices=tf.cast(tf.convert_to_tensor(fields[-1:]), tf.int32),
+                axis=0,
+            ),
+            (1, 100),
+        )
+        return fields[0], x, y
 
     def csv_reader_dataset(
-        self,
-        filenames,
-        batch_size=32,
-        n_parse_threads=5,
-        shuffle_buffer_size=10000,
-        n_readers=5,
+        self, filenames, batch_size=32, n_parse_threads=5, n_readers=5,
     ):
         """
         """
@@ -56,7 +61,6 @@ class deltascuti(Data):
             lambda filename: tf.data.TextLineDataset(filename).skip(0),
             cycle_length=n_readers,
         )
-        dataset = dataset.shuffle(shuffle_buffer_size)
         dataset = dataset.map(self.parse_csv_line, num_parallel_calls=n_parse_threads)
         dataset = dataset.batch(batch_size)
         return dataset.prefetch(1)
