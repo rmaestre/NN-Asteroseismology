c5cba81f1dfa2c0d1b504c6f955601fc6e08c82e
roberto <roberto.maestre@bbva.com>
2021-07-27 11:09:32 +0200

    Asimetric error on RM-2020 relation. New colorscheme added

--- astronn/utils/metrics.py
+++ astronn/utils/metrics.py
@@ -48,6 +48,19 @@ def get_rho(delta_nu):
     """
     return 1.6 * np.power(delta_nu, 2.02)
 
+def get_rho_upper_bound(delta_nu, e_A=0.5, e_B=0.1):
+    """
+    Relation Rodriguez-Martin et.al. 2020
+    """
+    return (1.6 + e_A) * np.power(delta_nu, (2.02 + e_B))
+
+
+def get_rho_lower_bound(delta_nu, e_A=0.5, e_B=0.1):
+    """
+    Relation Rodriguez-Martin et.al. 2020
+    """
+    return (1.6 - (e_A - 0.1)) * np.power(delta_nu, (2.02 - e_B))
+
 
 def rho_error(delta_nu, A=1.6, e_A=0.5, B=2.02, e_B=0.1):
     """
